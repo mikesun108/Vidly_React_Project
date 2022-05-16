4b71d5a8ef4e04c910489f1859032da5f8dc65f3
Michael Sundaresan <michaelsundaresan1008@gmail.com>
2022-05-16 02:01:34 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Movie from "./component/movie";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main>
+      <Movie />
+    </main>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
